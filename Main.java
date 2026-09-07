fa245e2e2807644dfadfb896591c9e5f701cd46b
Edinson Hernandez Cubillos <ehernandezc@poligran.edu.co>
2026-09-07 15:04:14 -0500

    Entry name correction

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        Scanner entrada = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = entrada.nextInt();
+        int number = userInput.nextInt();
         // A number is a multiple of 100 if the remainder of dividing it by 100 is 0
         if (number % 100 == 0){
             System.out.println("multiple of 100");
         } else {
             System.out.println("not multiple of 100");
         }
-        entrada.close();
+        userInput.close();
     }
 }
